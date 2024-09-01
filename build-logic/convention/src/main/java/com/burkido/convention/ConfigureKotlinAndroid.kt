bbd44b914d4fcf5b5e0ce1be03566d5a1287e87d
burak <karadumanburak000@gmail.com>
2024-09-01 10:56:13 +0300

    - BuildType configuration added

---
+++ build-logic/convention/src/main/java/com/burkido/convention/ConfigureKotlinAndroid.kt
@@ -0,0 +1,36 @@
+package com.burkido.convention
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *, *>
+) {
+    commonExtension.apply {
+        compileSdk = libs.findVersion("projectCompileSdkVersion").get().toString().toInt()
+        defaultConfig.minSdk = libs.findVersion("projectMinSdkVersion").get().toString().toInt()
+
+        compileOptions {
+            isCoreLibraryDesugaringEnabled = true
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+        configureKotlin()
+
+        dependencies {
+            "coreLibraryDesugaring"(libs.findLibrary("desugar.jdk.libs").get())
+        }
+    }
+}
+
+private fun Project.configureKotlin() {
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_11.toString()
+        }
+    }
+}
---
+++ build-logic/convention/src/main/java/com/burkido/convention/ExtensionType.kt
@@ -0,0 +1,6 @@
+package com.burkido.convention
+
+enum class ExtensionType {
+    APPLICATION,
+    LIBRARY
+}
