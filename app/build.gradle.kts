c10ddc6ea7027120549aaee9f13a2588994a7bea
burak <karadumanburak000@gmail.com>
2024-09-01 10:56:57 +0300

    - Removed unnecessary lines after adding build types

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,37 +5,14 @@ plugins {
 
 android {
     namespace = "com.burkido.runiqueclone"
-    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.burkido.runiqueclone"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     buildFeatures {
         compose = true
     }
--- build-logic/convention/src/main/java/com/burkido/convention/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.burkido.convention
-
-class MyClass {
-}
