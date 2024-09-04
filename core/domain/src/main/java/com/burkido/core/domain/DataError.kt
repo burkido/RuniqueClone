0589fc0513af0f56c553e187dda1ccd42ba56325
burak <karadumanburak000@gmail.com>
2024-09-04 18:09:50 +0300

    - TODO added

--- core/domain/src/main/java/com/burkido/core/domain/DataError.kt
+++ core/domain/src/main/java/com/burkido/core/domain/DataError.kt
@@ -1,6 +1,7 @@
 package com.burkido.core.domain
 
 // TODO: Think about location of this. Maybe it should be in the data layer because of @StringRes
+// Separating is a good idea maybe because we don't need that message while getting data from the data layer
 
 sealed interface DataError : Error {
 
