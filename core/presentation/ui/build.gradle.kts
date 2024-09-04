fd5d9e7e251cded1be7da93df57db5da6e59b0cc
burak <karadumanburak000@gmail.com>
2024-09-04 18:10:34 +0300

    - dependencies updated to use core.domain

--- core/presentation/ui/build.gradle.kts
+++ core/presentation/ui/build.gradle.kts
@@ -7,10 +7,13 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    api(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3)
+
+    implementation(projects.core.domain)
+    implementation(projects.core.presentation.designsystem)
 }
