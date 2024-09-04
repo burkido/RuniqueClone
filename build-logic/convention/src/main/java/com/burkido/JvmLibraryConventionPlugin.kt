232ff33c2fdf35fe5b0202a07a9f27bd9506a72d
burak <karadumanburak000@gmail.com>
2024-09-04 11:11:08 +0300

    - JvmLibraryConventionPlugin added

---
+++ build-logic/convention/src/main/java/com/burkido/JvmLibraryConventionPlugin.kt
@@ -0,0 +1,13 @@
+import com.burkido.convention.configureKotlinJvm
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class JvmLibraryConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.apply("org.jetbrains.kotlin.jvm")
+            configureKotlinJvm()
+        }
+    }
+}
