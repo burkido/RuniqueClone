9168a6ab09491e4079f8808a5571237a249cc013
burak <karadumanburak000@gmail.com>
2024-09-05 15:42:34 +0300

    - Color and theme added

---
+++ core/presentation/designsystem/src/main/java/com/burkido/core/presentation/designsystem/Color.kt
@@ -0,0 +1,15 @@
+package com.burkido.core.presentation.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+val RuniqueCloneGreen = Color(0xFF00F15E)
+val RuniqueCloneGreen30 = Color(0x4D00F15E)
+val RuniqueCloneGreen10 = Color(0x1A00F15E)
+val RuniqueCloneGreen5 = Color(0x0D00F15E)
+val RuniqueCloneBlack = Color(0xFF080707)
+val RuniqueCloneGray = Color(0xFF87938C)
+val RuniqueCloneGray40 = Color(0x6687938C)
+val RuniqueCloneDarkGray = Color(0xFF232624)
+val RuniqueCloneWhite = Color(0xFFFAFAFA)
+val RuniqueCloneDarkRed = Color(0xFFBB3D3D)
+val RuniqueCloneDarkRed5 = Color(0x0DBB3D3D)
