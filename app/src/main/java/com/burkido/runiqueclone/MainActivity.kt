501176b9d74657fea2a3a306829c20d2fe006839
burak <karadumanburak000@gmail.com>
2024-09-12 13:16:24 +0300

    - Removed unused imports

--- app/src/main/java/com/burkido/runiqueclone/MainActivity.kt
+++ app/src/main/java/com/burkido/runiqueclone/MainActivity.kt
@@ -2,15 +2,12 @@ package com.burkido.runiqueclone
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.burkido.core.presentation.designsystem.ui.RuniqueCloneTheme
