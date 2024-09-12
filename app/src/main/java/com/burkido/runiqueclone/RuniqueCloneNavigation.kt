63757a27b97b72ba9bee8380463d2bcdc7565b06
burak <karadumanburak000@gmail.com>
2024-09-12 10:25:24 +0300

    - Auth screens connected

--- app/src/main/java/com/burkido/runiqueclone/RuniqueCloneNavigation.kt
+++ app/src/main/java/com/burkido/runiqueclone/RuniqueCloneNavigation.kt
@@ -1,5 +1,6 @@
 package com.burkido.runiqueclone
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -7,17 +8,20 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import com.burkido.auth.presentation.intro.IntroScreenRoot
+import com.burkido.auth.presentation.login.LoginScreenRoot
 import com.burkido.auth.presentation.register.RegisterScreenRoot
 
 @Composable
 fun NavigationRoot(
-    navController: NavHostController
+    navController: NavHostController,
+    isLoggedIn: Boolean
 ) {
     NavHost(
         navController = navController,
-        startDestination = "auth"
+        startDestination = if (isLoggedIn) "run" else "auth"
     ) {
         authGraph(navController)
+        runGraph(navController)
     }
 }
 
@@ -29,10 +33,9 @@ private fun NavGraphBuilder.authGraph(navController: NavHostController) {
         composable("intro") {
             IntroScreenRoot(
                 onSignInClick = { navController.navigate("login") },
-                onSignUpClick = { navController.navigate("regişter") }
+                onSignUpClick = { navController.navigate("register") }
             )
         }
-
         composable("register") {
             RegisterScreenRoot(
                 onSignInClick = {
@@ -46,6 +49,37 @@ private fun NavGraphBuilder.authGraph(navController: NavHostController) {
                 },
                 onRegisteredSuccessfully = { navController.navigate("login") }
             )
+        }
+        composable("login") {
+            LoginScreenRoot(
+                onLoginSuccess = {
+                    navController.navigate("run") {
+                        popUpTo("auth") {
+                            inclusive = true
+                        }
+                    }
+                },
+                onSignUpClick = {
+                    navController.navigate("register") {
+                        popUpTo("login") {
+                            inclusive = true
+                            saveState = true
+                        }
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+private fun NavGraphBuilder.runGraph(navController: NavHostController) {
+    navigation(
+        startDestination = "run_overview",
+        route = "run"
+    ) {
+        composable("run_overview") {
+            Text(text = "Runke")
         }
     }
 }
