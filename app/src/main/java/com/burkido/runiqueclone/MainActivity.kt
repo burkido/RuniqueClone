a0c8713a6ed31194842bd0791c2ebeca759f2d85
burak <karadumanburak000@gmail.com>
2024-09-12 12:34:54 +0300

    - Bug fix

--- app/src/main/java/com/burkido/runiqueclone/MainActivity.kt
+++ app/src/main/java/com/burkido/runiqueclone/MainActivity.kt
@@ -2,12 +2,15 @@ package com.burkido.runiqueclone
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.burkido.core.presentation.designsystem.ui.RuniqueCloneTheme
@@ -19,13 +22,12 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         installSplashScreen().apply {
             setKeepOnScreenCondition { viewModel.state.isCheckingAuth }
         }
+        enableEdgeToEdge()
         setContent {
             RuniqueCloneTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
--- auth/presentation/src/main/java/com/burkido/auth/presentation/intro/IntroScreen.kt
+++ auth/presentation/src/main/java/com/burkido/auth/presentation/intro/IntroScreen.kt
@@ -1,7 +1,25 @@
 package com.burkido.auth.presentation.intro
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.burkido.auth.presentation.R
+import com.burkido.auth.presentation.intro.components.RuniqueCloneLogoVertical
+import com.burkido.core.presentation.designsystem.components.GradientBackground
+import com.burkido.core.presentation.designsystem.components.RuniqueCloneActionButton
+import com.burkido.core.presentation.designsystem.components.RuniqueCloneOutlinedActionButton
 import com.burkido.core.presentation.designsystem.ui.RuniqueCloneTheme
 
 @Composable
@@ -18,8 +36,55 @@ fun IntroScreenRoot(
 }
 
 @Composable
-fun IntroScreen(onAction: (IntroAction) -> Unit) {
-
+fun IntroScreen(
+    onAction: (IntroAction) -> Unit
+) {
+    GradientBackground {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            RuniqueCloneLogoVertical()
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .padding(bottom = 48.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.welcome_to_runique),
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(id = R.string.runique_description),
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            RuniqueCloneOutlinedActionButton(
+                text = stringResource(id = R.string.sign_in),
+                isLoading = false,
+                onClick = {
+                    onAction(IntroAction.OnSignInClick)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            RuniqueCloneActionButton(
+                text = stringResource(id = R.string.sign_up),
+                isLoading = false,
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    onAction(IntroAction.OnSignUpClick)
+                }
+            )
+        }
+    }
 }
 
 @Preview
