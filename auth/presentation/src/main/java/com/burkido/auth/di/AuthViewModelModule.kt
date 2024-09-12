87dce655970c9995531fc943e097fec26e8e9c6a
burak <karadumanburak000@gmail.com>
2024-09-12 12:43:47 +0300

    - Bug fix: LoginViewModel added to module list

--- auth/presentation/src/main/java/com/burkido/auth/di/AuthViewModelModule.kt
+++ auth/presentation/src/main/java/com/burkido/auth/di/AuthViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.burkido.auth.di
 
+import com.burkido.auth.presentation.login.LoginViewModel
 import com.burkido.auth.presentation.register.RegisterViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val authViewModelModule = module {
+    viewModelOf(::LoginViewModel)
     viewModelOf(::RegisterViewModel)
 }
