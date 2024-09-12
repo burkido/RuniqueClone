749f95b6259473123cf688b7b22f6742f0c0cfb6
burak <karadumanburak000@gmail.com>
2024-09-12 12:34:49 +0300

    - RuniqueCloneLogoVertical added

---
+++ auth/presentation/src/main/java/com/burkido/auth/presentation/intro/components/RuniqueCloneLogoVertical.kt
@@ -0,0 +1,40 @@
+package com.burkido.auth.presentation.intro.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.burkido.auth.presentation.R
+import com.burkido.core.presentation.designsystem.ui.LogoIcon
+
+@Composable
+fun RuniqueCloneLogoVertical(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = LogoIcon,
+            contentDescription = "Logo",
+            tint = MaterialTheme.colorScheme.onBackground
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = stringResource(id = R.string.runique),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
