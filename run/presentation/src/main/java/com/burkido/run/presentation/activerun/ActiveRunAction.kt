8b784226e3045873d0972c379719f09300c630f7
burak <karadumanburak000@gmail.com>
2024-09-16 16:35:14 +0300

    - New actions added

--- run/presentation/src/main/java/com/burkido/run/presentation/activerun/ActiveRunAction.kt
+++ run/presentation/src/main/java/com/burkido/run/presentation/activerun/ActiveRunAction.kt
@@ -5,4 +5,15 @@ sealed interface ActiveRunAction {
     data object OnFinishRunClick : ActiveRunAction
     data object OnResumeRunClick : ActiveRunAction
     data object OnBackClick : ActiveRunAction
+    data class SubmitLocationPermissionInfo(
+        val grantedLocationPermission: Boolean,
+        val showLocationRationale: Boolean
+    ) : ActiveRunAction
+
+    data class SubmitNotificationPermissionInfo(
+        val grantedNotificationPermission: Boolean,
+        val showNotificationPermissionRationale: Boolean
+    ) : ActiveRunAction
+
+    data object DismissRationaleDialog : ActiveRunAction
 }
