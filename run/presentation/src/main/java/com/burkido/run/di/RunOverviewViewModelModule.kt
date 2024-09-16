a2e234467863a07c7801f48ddf77c3273743b9f7
burak <karadumanburak000@gmail.com>
2024-09-16 16:36:56 +0300

    - ActiveRunViewModel added into run module

--- run/presentation/src/main/java/com/burkido/run/di/RunOverviewViewModelModule.kt
+++ run/presentation/src/main/java/com/burkido/run/di/RunOverviewViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.burkido.run.di
 
+import com.burkido.run.presentation.activerun.ActiveRunViewModel
 import com.burkido.run.presentation.runoverview.RunOverviewViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val runOverviewViewModelModule = module {
     viewModelOf(::RunOverviewViewModel)
+    viewModelOf(::ActiveRunViewModel)
 }
