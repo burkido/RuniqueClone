34cffebdc1bdc7fa848fa08ed4d9128d72b8d65b
burak <karadumanburak000@gmail.com>
2024-09-23 16:33:03 +0300

    - Added missing functionalities to ActiveRunViewModel.kt

--- run/presentation/src/main/java/com/burkido/run/presentation/activerun/ActiveRunViewModel.kt
+++ run/presentation/src/main/java/com/burkido/run/presentation/activerun/ActiveRunViewModel.kt
@@ -3,12 +3,21 @@ package com.burkido.run.presentation.activerun
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.burkido.run.domain.model.RunningTracker
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 
 class ActiveRunViewModel(
-
+    private val runningTracker: RunningTracker
 ) : ViewModel() {
 
     var state by mutableStateOf(ActiveRunState())
@@ -17,24 +26,53 @@ class ActiveRunViewModel(
     private val _events = Channel<ActiveRunEvent>()
     val events = _events.receiveAsFlow()
 
+    private val snapshotShouldTrack = snapshotFlow { state.shouldTrack }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Lazily,
+        initialValue = state.shouldTrack
+    )
+
+    private val hasLocationPermission = MutableStateFlow(false)
+
+    private val isTracking = combine(
+        snapshotShouldTrack,
+        hasLocationPermission
+    ) { shouldTrack, hasLocationPermission ->
+        shouldTrack && hasLocationPermission
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Lazily,
+        initialValue = false
+    )
+
+    init {
+        hasLocationPermission
+            .onEach { runningTracker.apply { if (it) startObservingLocation() else stopObservingLocation() } }
+            .launchIn(viewModelScope)
+
+        isTracking
+            .onEach { runningTracker.setIsTracking(it) }
+            .launchIn(viewModelScope)
+
+        runningTracker.apply {
+            currentLocation.onEach { state = state.copy(currentLocation = it?.location) }
+                .launchIn(viewModelScope)
+            runData.onEach { state = state.copy(runData = it) }.launchIn(viewModelScope)
+            elapsedTime.onEach { state = state.copy(elapsedTime = it) }.launchIn(viewModelScope)
+        }
+    }
+
     fun onAction(action: ActiveRunAction) {
         when (action) {
-            ActiveRunAction.DismissRationaleDialog -> {
-                dismissRationaleDialog()
-            }
-            ActiveRunAction.OnBackClick -> TODO()
-            ActiveRunAction.OnFinishRunClick -> TODO()
-            ActiveRunAction.OnResumeRunClick -> TODO()
-            ActiveRunAction.OnToggleRunClick -> {
-
-            }
-            is ActiveRunAction.SubmitLocationPermissionInfo -> {
-                submitLocationPermissionInfo(action.showLocationRationale)
-            }
-
-            is ActiveRunAction.SubmitNotificationPermissionInfo -> {
-                submitNotificationPermissionInfo(action.showNotificationPermissionRationale)
-            }
+            ActiveRunAction.DismissRationaleDialog -> dismissRationaleDialog()
+            ActiveRunAction.OnBackClick -> onBackClick()
+            ActiveRunAction.OnFinishRunClick -> onFinishRunClick()
+            ActiveRunAction.OnResumeRunClick -> onResumeRunClick()
+            ActiveRunAction.OnToggleRunClick -> onToggleRunClick()
+            is ActiveRunAction.SubmitLocationPermissionInfo -> submitLocationPermissionInfo(action.showLocationRationale)
+            is ActiveRunAction.SubmitNotificationPermissionInfo -> submitNotificationPermissionInfo(
+                action.showNotificationPermissionRationale
+            )
         }
     }
 
@@ -42,6 +80,25 @@ class ActiveRunViewModel(
         state = state.copy(
             showNotificationRationale = false,
             showLocationRationale = false
+        )
+    }
+
+    private fun onBackClick() {
+        state = state.copy(shouldTrack = false)
+    }
+
+    private fun onFinishRunClick() {
+        TODO("Not yet implemented")
+    }
+
+    private fun onResumeRunClick() {
+        state = state.copy(shouldTrack = true)
+    }
+
+    private fun onToggleRunClick() {
+        state = state.copy(
+            shouldTrack = !state.shouldTrack,
+            hasStartedRunning = true    // TODO: consider true only when the user starts running
         )
     }
 
