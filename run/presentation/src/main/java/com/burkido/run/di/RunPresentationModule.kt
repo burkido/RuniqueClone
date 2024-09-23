f96086a7d1bc01d0f8e7d101c3f831c48b999e9b
burak <karadumanburak000@gmail.com>
2024-09-23 16:33:21 +0300

    - Module name updated

--- run/presentation/src/main/java/com/burkido/run/di/RunPresentationModule.kt
+++ run/presentation/src/main/java/com/burkido/run/di/RunPresentationModule.kt
@@ -3,9 +3,12 @@ package com.burkido.run.di
 import com.burkido.run.presentation.activerun.ActiveRunViewModel
 import com.burkido.run.presentation.runoverview.RunOverviewViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
-val runOverviewViewModelModule = module {
+val runPresentationModule = module {
+    singleOf(::RunOverviewViewModel)
+
     viewModelOf(::RunOverviewViewModel)
     viewModelOf(::ActiveRunViewModel)
 }
