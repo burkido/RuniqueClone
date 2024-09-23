a0f4968605809ce5507f5cd82c404cffb212b213
burak <karadumanburak000@gmail.com>
2024-09-23 16:32:16 +0300

    - Location observer provided with module

---
+++ run/location/src/main/java/com/burkido/run/di/LocationModule.kt
@@ -0,0 +1,11 @@
+package com.burkido.run.di
+
+import com.burkido.run.domain.LocationObserver
+import com.burkido.run.location.AndroidLocationObserver
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+val locationModule = module {
+    singleOf(::AndroidLocationObserver).bind<LocationObserver>()
+}
