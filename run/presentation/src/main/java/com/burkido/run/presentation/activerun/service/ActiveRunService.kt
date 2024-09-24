4aacbc71b054d8dd759169970ac5d46c5b9acfbf
burak <karadumanburak000@gmail.com>
2024-09-24 16:55:27 +0300

    - ActiveRunService added

---
+++ run/presentation/src/main/java/com/burkido/run/presentation/activerun/service/ActiveRunService.kt
@@ -0,0 +1,138 @@
+package com.burkido.run.presentation.activerun.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.app.TaskStackBuilder
+import androidx.core.content.getSystemService
+import androidx.core.net.toUri
+import com.burkido.core.presentation.ui.formatted
+import com.burkido.run.domain.model.RunningTracker
+import com.burkido.run.presentation.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import org.koin.android.ext.android.inject
+
+class ActiveRunService : Service() {
+
+    private val notificationManager by lazy {
+        getSystemService<NotificationManager>()!!
+    }
+
+    private val baseNotification by lazy {
+        NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(com.burkido.core.presentation.designsystem.R.drawable.logo)
+            .setContentTitle(getString(R.string.active_run))
+    }
+
+    private val runningTracker by inject<RunningTracker>()
+
+    private var serviceScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
+            ACTION_START -> {
+                val activityClass = intent.getStringExtra(EXTRA_ACTIVITY_CLASS)
+                    ?: throw IllegalArgumentException("No activity class provided")
+                start(Class.forName(activityClass))
+            }
+
+            ACTION_STOP -> actionStop()
+        }
+        return START_STICKY
+    }
+
+    private fun actionStop() {
+        stopSelf()
+        isServiceActive = false
+        serviceScope.cancel()
+
+        serviceScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
+    }
+
+    private fun start(activityClass: Class<*>) {
+        if (!isServiceActive) {
+            isServiceActive = true
+            createNotificationChannel()
+
+            val activityIntent = Intent(applicationContext, activityClass).apply {
+                data = "runiqueclone://active_run".toUri()
+                addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            }
+
+            val pendingIntent = TaskStackBuilder.create(applicationContext).run {
+                addNextIntentWithParentStack(activityIntent)
+                getPendingIntent(0, PendingIntent.FLAG_IMMUTABLE)
+            }
+
+            val notification = baseNotification
+                .setContentText("00:00:00")
+                .setContentIntent(pendingIntent)
+                .build()
+
+            startForeground(1, notification)
+            updateNotification()
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= 26) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                getString(R.string.active_run),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
+    private fun updateNotification() {
+        runningTracker.elapsedTime.onEach { elapsedTime ->
+            val notification = baseNotification
+                .setContentText(elapsedTime.formatted())
+                .build()
+
+            notificationManager.notify(1, notification)
+        }.launchIn(serviceScope)
+    }
+
+
+    companion object {
+        private const val CHANNEL_ID = "ActiveRunService"
+
+        private val ACTION_START = "ACTION_START"
+        private val ACTION_STOP = "ACTION_STOP"
+
+        private val EXTRA_ACTIVITY_CLASS = "EXTRA_ACTIVITY_CLASS"
+
+        var isServiceActive = false
+
+        fun createStartIntent(context: Context, activityClass: Class<*>): Intent {
+            return Intent(context, ActiveRunService::class.java).apply {
+                action = ACTION_START
+                putExtra(EXTRA_ACTIVITY_CLASS, activityClass.name)
+            }
+        }
+
+        fun stopIntent(context: Context): Intent {
+            return Intent(context, ActiveRunService::class.java).apply {
+                action = ACTION_STOP
+            }
+        }
+    }
+}
